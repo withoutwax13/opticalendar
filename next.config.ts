78dad3786d1a210ed1d5c94d0be4c6d02f6c01e9
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2025-01-29 00:25:17 +0800

    Add TypeScript configuration to ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
