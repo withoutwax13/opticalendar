127a24325c7e9d3d66ba2559fd9551a19d1e8fa5
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2025-02-02 17:58:21 +0800

    Refactor change log handling and improve server sync logic in App component

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -16,10 +16,12 @@ import storeProvider from "@/components/StoreProvider";
 import Loader from "@/components/Loader";
 
 import { handleServerSync } from "@/utils";
+import { ChangeLogType, ScheduleData } from "@/types";
 
 const App = (props) => {
   const { rehydrated } = props;
   const [isLoading, setIsLoading] = useState(true);
+  const [initialSyncToServer, setInitialSyncToServer] = useState(false);
 
   const userInfo = useSelector((state) => state.userInfo.userInfo) || null;
   const clientSchedules =
@@ -40,30 +42,74 @@ const App = (props) => {
     };
 
     // Syncing strategy:
-    // 1. Sync changes to the server if the changeLogs length is different from the previousChangeLogLength and is divisible by 3.
+    // 1. Sync changes to the server if the changeLogs length is different from the previousChangeLogLength and is divisible by 5.
     // 2. If the user closed the browser before syncing, sync the changes when the app loads again by checking if changeLogs has items.
+    // 3. If the changeLogs has a delete type of change, sync the changes to the server without waiting fo the isIncrementalConditionTrue to be true.
+    // 4. Check if changeLogs has a delete type and other types of the same ID, do not execute the add/edit type of change, since it will be deleted anyway.
 
     const isReloadedWithoutLogout =
-        previousChangeLogLength === -1 && changeLogs.length > 0,
-      isIncrementalConditionTrue =
-        changeLogs.length !== previousChangeLogLength &&
-        changeLogs.length % 3 === 0;
+      previousChangeLogLength === -1 && changeLogs.length > 0;
+    const isIncrementalConditionTrue =
+      changeLogs.length !== previousChangeLogLength &&
+      changeLogs.length % 5 === 0;
+    const hasDeleteChange = changeLogs.some(
+      (changeLog: ChangeLogType) => changeLog.type === "delete"
+    );
 
     if (rehydrated) {
       if (!userInfo) {
         window.location.href = "/login";
-      } else if (clientSchedules.length === 0) {
+      } else if (clientSchedules.length === 0 && !initialSyncToServer) {
+        setInitialSyncToServer(true);
         updateClientSchedules();
       } else {
         setIsLoading(false);
       }
     }
 
+    // if (hasDeleteChange) {
+    //   const isDeleteItemHasOtherTypes = changeLogs.some(
+    //     (changeLog: ChangeLogType) => {
+    //       return changeLog.type !== "delete" && changeLog.id === changeLog.id;
+    //     }
+    //   );
+    //   console.log("isDeleteItemHasOtherTypes: ", isDeleteItemHasOtherTypes);
+    //   setIsLoading(true);
+    //   try {
+    //     if (!isDeleteItemHasOtherTypes) {
+    //       changeLogs.forEach((changeLog: ChangeLogType) => {
+    //         handleServerSync(changeLog).catch(() => {
+    //           toast.error("Server sync failed");
+    //           setIsLoading(false);
+    //         });
+    //       });
+    //     } else {
+    //       changeLogs.forEach((changeLog: ChangeLogType) => {
+    //         if (changeLog.type !== "delete") {
+    //           handleServerSync(changeLog).catch(() => {
+    //             toast.error("Server sync failed");
+    //             setIsLoading(false);
+    //           });
+    //         }
+    //       });
+    //     }
+    //   } catch (error: any) {
+    //     toast.error("Server sync failed");
+    //     setIsLoading(false);
+    //   } finally {
+    //     dispatch(clearChangeLog());
+    //     setPreviousChangeLogLength(changeLogs.length);
+    //     updateClientSchedules();
+    //     setIsLoading(false);
+    //   }
+    // }
+    console.log("changeLogs -> ", changeLogs);
+
     if (isReloadedWithoutLogout || isIncrementalConditionTrue) {
       setIsLoading(true);
       try {
-        changeLogs.forEach((changeLog) => {
-          handleServerSync(changeLog).catch((error) => {
+        changeLogs.forEach((changeLog: ChangeLogType) => {
+          handleServerSync(changeLog).catch(() => {
             toast.error("Server sync failed");
             setIsLoading(false);
           });
@@ -74,6 +120,7 @@ const App = (props) => {
       } finally {
         dispatch(clearChangeLog());
         setPreviousChangeLogLength(changeLogs.length);
+        updateClientSchedules();
         setIsLoading(false);
       }
     }
@@ -85,6 +132,7 @@ const App = (props) => {
     changeLogs,
     changeLogs.length,
     previousChangeLogLength,
+    initialSyncToServer,
     dispatch,
   ]);
 
@@ -95,7 +143,7 @@ const App = (props) => {
       <Header user={userInfo} />
       <div className="calendar px-5">
         <MainCalendar
-          events={clientSchedules.map((schedule) => {
+          events={clientSchedules.map((schedule: ScheduleData) => {
             return {
               ...schedule.schedule_data,
               id: schedule.id,
